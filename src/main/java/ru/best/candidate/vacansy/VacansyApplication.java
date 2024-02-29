dc127c67230d5e98d5d90f434c76962bef500a5e
Shapoval Denis <speleo04@yandex.ru>
2024-02-29 12:12:03 +0300

    init project

---
+++ src/main/java/ru/best/candidate/vacansy/VacansyApplication.java
@@ -0,0 +1,13 @@
+package ru.best.candidate.vacansy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacansyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacansyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/best/candidate/vacansy/VacansyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.best.candidate.vacansy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacansyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
